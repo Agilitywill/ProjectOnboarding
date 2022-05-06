724bedc331bb30aa2c052597fe4091e007d43b69
Agilitywill <99764135+Agilitywill@users.noreply.github.com>
2022-05-05 23:58:50 -0400

    Update Intersection.java

--- src/weekOne/Intersection.java
+++ src/weekOne/Intersection.java
@@ -1,5 +1,37 @@
 package weekOne;
 
 public class Intersection {
+	
 
+	@SuppressWarnings("null")
+	public Node getIntersection(Node headA, Node headB) {
+		
+		if(headA == null || headB == null) {
+			return null; 
+		}
+		
+		Node pointerA = headA; 
+		Node pointerB = headB; 
+		
+		while(pointerA != pointerB) {
+			
+			if(pointerA != null) {
+				pointerA = headB; 
+				
+			} else {
+				pointerA = pointerA.getNext(); 
+			}
+			
+			if(pointerB != null) {
+				pointerB = headA; 
+				
+			} else {
+				pointerB = pointerB.getNext(); 
+			}
+			
+		}
+		
+		return pointerA; 
+	}
+	
 }
