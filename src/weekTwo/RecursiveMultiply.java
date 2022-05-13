11504ee9c41266b69447121c3d31f028da619247
Agilitywill <99764135+Agilitywill@users.noreply.github.com>
2022-05-13 01:13:39 -0400

    Update RecursiveMultiply.java

--- src/weekTwo/RecursiveMultiply.java
+++ src/weekTwo/RecursiveMultiply.java
@@ -1,5 +1,10 @@
 package weekTwo;
 
+/*
+ * Write a recursive function to multiply two positive integers without using the *operator. 
+ * You can use addition, subtraction, and bit shifting, but you should minimize the number of those operations.
+ */
+
 public class RecursiveMultiply {
 	
 	public static void main(String[] args) {
